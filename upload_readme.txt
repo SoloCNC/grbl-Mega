e1a148ab9e85f37b09720bae70d853fbe5979079
linanw <linanwang@msn.com>
2020-09-22 21:50:49 +0800

    Windows cmd script for upload via Arduino

---
+++ upload_readme.txt
@@ -0,0 +1,5 @@
+Compile & Upload in Windows.
+1. Download Arduino IDE Desktop edition from https://arduino.cc.
+2. Run upload_via_arduino.cmd <com#> file, if your Documents folder is synced in OneDrive, run the upload_via_arduino_in_onedrive.cmd instead.
+
+e.g upload_via_arduino.cmd COM5
---
+++ upload_via_arduino_in_onedrive.cmd
@@ -0,0 +1,4 @@
+@echo off
+xcopy /e /y /q grbl\ %USERPROFILE%\OneDrive\Documents\Arduino\libraries\grbl\ > nul
+echo Opening Arduino IDE to compile and upload via serial port set in Arduino IDE...
+"c:\Program Files (x86)\Arduino\arduino.exe" --upload "%OneDrive%\Documents\Arduino\libraries\grbl\examples\grblUpload\grblUpload.ino" --board arduino:avr:mega --port %1
